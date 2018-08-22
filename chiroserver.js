03f764340ef9c1bed1ad6a0add6d5a2ba3213d3d
travis <tsdubose@gmail.com>
2018-08-22 09:24:11 -0400

    version .1: working on local host

---
+++ chiroserver.js
@@ -0,0 +1,118 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// TODO: Use socket.io's hashes to refer to clients in the state object.
+// TODO: Need to check and see if the video has loaded for both videos.
+// TODO: Need to look for places to resync.
+var state = {};
+state.twoConnected = false;
+state.twoReady = false;
+state.introPlaying = false;
+state.gamePlaying = false;
+state.hasVideo = null;
+state.hasAudio = null;
+state.gameOver = false;
+var players = [];
+var currentAudio = null;
+var currentVideo = null;
+
+
+//// TODO: Rather than using players[0] and 2, just push things into an array and refer to players[0] etc.
+io.on('connection', function(socket){
+	console.log(socket.id + ' connected');
+
+	socket.on("space", function (playerID) {
+		if (!state.twoReady) {
+			if (!players.includes(playerID)) {
+				players.push(playerID);
+				state[playerID] = {
+					ready: true,
+					finishedIntro: false,
+					playingIntro: false,
+					gamePlaying: false,
+					gameOver: false
+				};
+				console.log(playerID + " is ready.");
+				io.to(playerID).emit("changescreen");
+				if (players.length == 2 && (state[players[0]].ready == true && state[players[1]].ready == true)) {
+					state.twoReady = true;
+					console.log("Two clients are ready. Starting intro video.");
+					state.hasAudio = players[0];
+					state.hasVideo = players[1];
+					console.log("Starting roles assigned. " + players[0] + " has audio and " + players[1] + " has video.");
+					io.of('/').emit("playintro");
+				}
+
+			}
+			// if (players.length == 2) {
+			// 	state.twoConnected = true;
+			// }
+		}
+		else if (state.gamePlaying) {
+			theSwitch();
+		}
+	});
+
+	socket.on("instructionsplaying", function (playerID) {
+		state[playerID].playingIntro = true;
+		if (state[players[0]].playingIntro && state[players[1]].playingIntro) {
+			state.introPlaying = true;
+			console.log("Both players are confirmed to be playing intro.");
+		}
+	});
+
+	socket.on("instructionsover", function (playerID) {
+		state[playerID].finishedIntro = true;
+		console.log(playerID + " has finished the intro.");
+		if (state[players[0]].finishedIntro && state[players[1]].finishedIntro) {
+			console.log("Both players have finished the intro video, starting the game.");
+			//Tell them to prepare to mute audio and video.
+			io.to(state.hasAudio).emit("mutevideo");
+			io.to(state.hasVideo).emit("muteaudio");
+			io.of('/').emit("playgame");
+		}
+	});
+	socket.on("gameplaying", function (playerID) {
+		state[playerID].gamePlaying = true;
+		if (state[players[0]].gamePlaying && state[players[1]].gamePlaying) {
+			console.log("Both players have confirmed game video is playing.");
+			state.gamePlaying = true;
+		}
+	});
+
+	socket.on("gameover", function (playerID) {
+		state[playerID].gameOver = true;
+		console.log(playerID + " has ended the game.");
+		if (state[players[0]].gameOver && state[players[1]].gameOver) {
+			state.gameOver = true;
+			console.log("Both players have finished, resetting server.");
+			io.of("/").emit("hardreset");
+			setTimeout(function () {
+				process.exit();
+			}, 200);
+		}
+	});
+});
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/client/chirointerface.html');
+});
+app.use(express.static('client'))
+http.listen(3000, function(){
+	console.log('Server up, listening on *:3000');
+});
+
+function theSwitch() {
+	currentAudio = state.hasAudio;
+	currentVideo = state.hasVideo;
+
+	io.to(state.hasAudio).emit("muteaudio");
+	io.to(state.hasVideo).emit("mutevideo");
+
+	state.hasAudio = currentVideo;
+	state.hasVideo = currentAudio;
+
+	console.log("Switch performed: " + state.hasAudio + " has audio and " + state.hasVideo + " has video.");
+}
---
+++ client/chiroclient.js
@@ -0,0 +1,65 @@
+var socket = io();
+var instructionPlayer = document.getElementsByClassName("instructions")[0];
+var gamePlayer = document.getElementsByClassName("game")[0];
+
+socket.on("changepic", function () {
+	$(".welcomescreen").hide();
+	$(".awaiting-screen").show();
+});
+
+window.addEventListener("keydown", function (e) {
+	if (e.key == " ") {
+		e.preventDefault();
+		socket.emit("space", socket.id);
+		console.log("message sent");
+		console.log(socket.id);
+	}
+});
+
+socket.on("changescreen", function () {
+	$(".welcome-screen").hide();
+	$(".awaiting-screen").show();
+});
+
+socket.on("playintro", function () {
+	$(".awaiting-screen").hide();
+	$(".instructions").show();
+	instructionPlayer.play();
+	if (!instructionPlayer.paused) {
+		socket.emit("instructionsplaying", socket.id);
+	}
+});
+
+socket.on("playgame", function () {
+	// TODO: Unload the intro video from memory here.
+	$(".instructions").hide();
+	$(".game").show();
+	gamePlayer.play();
+	if (!gamePlayer.paused) {
+		socket.emit("gameplaying", socket.id);
+	}
+});
+
+instructionPlayer.addEventListener("ended", function (e) {
+	socket.emit("instructionsover", socket.id);
+});
+
+socket.on("muteaudio", function () {
+	gamePlayer.muted = true;
+	$(".bigblackbox").hide();
+});
+
+socket.on("mutevideo", function () {
+	$(".bigblackbox").show();
+	gamePlayer.muted = false;
+});
+
+gamePlayer.addEventListener("ended", function (e) {
+	socket.emit("gameover", socket.id);
+});
+
+socket.on("hardreset", function () {
+	setTimeout(function () {
+		location.reload(true);
+	}, 2000);
+});
